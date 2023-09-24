73e7684a2185add434781bf33a6ba3efe1b9279b
IS <132925394+IsmoilSayfitdinov@users.noreply.github.com>
2023-09-24 22:21:36 +0500

    Update login.jsx

--- src/routes/login/login.jsx
+++ src/routes/login/login.jsx
@@ -4,10 +4,8 @@ const Login = () => {
   return (
     <div className='mainAuthLog'>
         <div className="mainImg__logo">
-<        davron-dev
           <h1>bbb</h1
            <h1>aaa</h1>
-       main
         </div>
     </div>
   )
