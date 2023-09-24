8a449f257a53b752c79e7ef4ded52da44a781a71
Karimboyevcmbk <Karimboyevcmbk.com>
2023-09-24 20:10:51 +0500

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
+import { Route,Routes } from 'react-router-dom'
+import Login from "./routes/login/Login"
+import LoginEmail from "./routes/loginEmail/LoginEmail"
 
 function App() {
 
   return (
-    <>
-     salom
-    </>
+    <div>
+      <Routes>
+        <Route path='/' element={<Login/>}/>
+        <Route path='/' element={<LoginEmail/>}/>
+      </Routes>
+    </div>
   )
 }
 
--- src/routes/login/login.jsx
+++ src/routes/login/login.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const login = () => {
   return (
-    <div>login</div>
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta molestiae animi sapiente dicta, sit blanditiis nobis qui impedit doloremque harum aut voluptatibus, velit cumque perferendis laudantium dignissimos esse autem quos.</div>
   )
 }
 
