e6c39efc9d6a6e06645fed8af37470a8c07496cc
Karimboyevcmbk <Karimboyevcmbk.com>
2023-09-24 21:29:23 +0500

    added login-email

--- src/routes/loginEmail/LoginEmail.jsx
+++ src/routes/loginEmail/LoginEmail.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import "./loginemail.scss"
+import loginImg from "../../assets/loginImg.svg"
+import loginPhoneImg from "../../assets/loginPhoneImg.svg"
 
 const LoginEmail = () => {
   return (
-    <div>LoginEmail</div>
+    <div className="container">
+      <div className="login-email">
+        <div className="login-email__section1">
+          <img src={loginImg} alt="" />
+          <strong>Fitnes</strong>
+          <p>qisqacha ta'rif</p>
+          <div className="dumoloqlar">
+            <div className="dumaloq"></div>
+            <div className="dumaloq1"></div>         
+            <div className="dumaloq"></div>       
+          </div>
+        </div>
+        <div className="login-email__section2">
+          <h1>Second Step Verification</h1>
+          <img src={loginPhoneImg} alt="" />
+          <p>Enter the verification code we sent to<br/><a href="#">matrix@gmail.com</a></p>
+          <form className="login-email__form">
+            <input type="text" placeholder="type code here"/>
+            <button>Submit</button>
+          </form>
+          
+        </div>
+      </div>
+    </div>
   )
 }
 
