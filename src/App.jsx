8b589bb05cd851203300f00c3bce63210599d923
IsmoilSayfitdinov <durdonairgashova489@gmail.com>
2023-09-26 10:07:00 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
-import React from 'react'
 import './App.css'
 import { Route,Routes } from 'react-router-dom'
 import Login from "./routes/login/Login"
 import LoginEmail from "./routes/loginEmail/LoginEmail"
 import Admin from './routes/admin/Admin'
+import Ustoz from './routes/ustozlar/Ustoz'
+import AddneUs from './routes/addnew/AddneUs'
 
 function App() {
 
   return (
-    <div className='container'>
-      <Routes>
-        <Route path='/' element={<Login/>}/>
-        <Route path='/loginemail' element={<LoginEmail/>}/>
-        <Route path='/admin' element={<Admin/>}/>
-      </Routes>
-    </div>
-  )
+		<div className='container'>
+			<Routes>
+				<Route path='/' element={<Login />} />
+				<Route path='/loginemail' element={<LoginEmail />} />
+				<Route path='/admin' element={<Admin />} />
+				<Route path='/ustozlar' element={<Ustoz />} />
+				<Route path='/addNew' element={<AddneUs/>}/>
+			</Routes>
+		</div>
+	)
 }
 
 export default App
