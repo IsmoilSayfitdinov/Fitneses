0ffcac666feb7cc86af829612866fb4200ce6a84
Karimboyevcmbk <Karimboyevcmbk.com>
2023-09-26 10:21:25 +0500

    added jihozlar

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import LoginEmail from "./routes/loginEmail/LoginEmail"
 import Admin from './routes/admin/Admin'
 import Teachers from './routes/teachers/Teachers'
 import Mijozlar from './routes/mijozlar/Mijozlar'
+import Jihozlar from './routes/jihozlar/Jihozlar'
 
 function App() {
 
@@ -17,6 +18,7 @@ function App() {
         <Route path='/admin' element={<Admin/>}/>
         <Route path='/teachers' element={<Teachers/>}/>
         <Route path='/mijozlar' element={<Mijozlar/>}/>
+        <Route path='/jihozlar' element={<Jihozlar/>}/>
       </Routes>
     </div>
   )
